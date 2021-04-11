d7581d40960221c510020b104c317b2e47145509
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-04-11 10:53:46 +0530

    Update Projects.js

--- pages/posts/Projects.js
+++ pages/posts/Projects.js
@@ -25,7 +25,7 @@ export default function Projects({data}){
 }
 
 export async function getStaticProps(){
-    const res = await fetch('http://localhost:5000/api/getproject')
+    const res = await fetch('https://mahadevan.herokuapp.com/api/getproject')
     const data = await res.json()
     console.log(data)
     if(!data){
