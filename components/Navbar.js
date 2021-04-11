7ce527d0c90e045feee60451a1d80ae40095fde5
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-04-11 11:00:59 +0530

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,45 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Navbar(){
+    return ( 
+      <div className={styles.navbar}>
+      <div className={styles.ttitle}>
+         <h2>
+            <Link href="/">
+                <a>devApp</a>
+            </Link>
+         </h2>
+      </div>
+      <div className={styles.menu}>
+          <ul className={styles.ul1}>
+              <h2>
+              <li>
+                  <Link href="/posts/Projects">
+                      <a>projects</a>
+                  </Link>
+              </li>
+              </h2>
+              <li>
+                  <h2>
+                    <li>
+                        <Link href="/posts/Blogs">
+                            <a>blogs</a>
+                        </Link>
+                    </li>
+                  </h2>
+                </li>
+              <li>
+                  <h2>
+                    <li>
+                        <Link href="/posts/Contact">
+                            <a>contact</a>
+                        </Link>
+                    </li>
+                  </h2>
+              </li>
+          </ul>
+      </div>
+    </div>
+    )
+}
