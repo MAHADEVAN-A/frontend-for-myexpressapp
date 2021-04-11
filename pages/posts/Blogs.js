cb847df0cab9487b20cac4c6fcbd4bffe2eb286d
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-04-11 12:36:27 +0530

    Update Blogs.js

--- pages/posts/Blogs.js
+++ pages/posts/Blogs.js
@@ -25,7 +25,7 @@ export default function Blogs({data}){
 }
 
 export async function getStaticProps(){
-    const res = await fetch('http://localhost:5000/api/getblog')
+    const res = await fetch('https://mahadevan.herokuapp.com/api/getblog')
     const data = await res.json()
     console.log(data)
     if(!data){
