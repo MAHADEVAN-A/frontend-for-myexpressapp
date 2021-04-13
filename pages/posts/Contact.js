bfd89ad14ffa76674d01b4cafcdf4a1eb552d98f
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-04-13 20:57:48 +0530

    Add files via upload

--- pages/posts/Contact.js
+++ pages/posts/Contact.js
@@ -1,11 +1,12 @@
 import styles from '../../styles/Home.module.css'
-// import Image from 'next/image'
+import Image from 'next/image'
 // import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/Layout'
 
 export default function Contact({data}){
     const url = `https://mahadevan.herokuapp.com/assets/contact/contact.svg`
+    // const url = `http://localhost:5000/assets/contact/contact.svg`
     return (
         <Layout>
             <Head>
@@ -24,7 +25,7 @@ export default function Contact({data}){
                     <button type="submit">send</button>
                 </form>
                 <div className='form-cimage'>
-                    <img src={url}/>
+                    <ImageComp isrc={url} />
                 </div>
                 <div className='form1'>
                     <div className='form-cont1'>
@@ -41,6 +42,7 @@ export default function Contact({data}){
 
 export async function getStaticProps(){
     const res = await fetch('https://mahadevan.herokuapp.com/api/getcontact')
+    // const res = await fetch('http://localhost:5000/api/getcontact')
     const data = await res.json()
     console.log(data)
     if(!data){
@@ -53,13 +55,14 @@ export async function getStaticProps(){
         revalidate: 60,
     }
 }
-// const ImageComp= ()=>{
-//     return(
-//         <Image 
-//         src="/images/freelancer.svg"
-//         height={200}
-//         width={200}
-//         alt="mahadevan"
-//         />
-//     )
-// }
+const ImageComp= (props)=>{
+    const url = props.isrc;
+    return(
+        <Image 
+        src={url}
+        height={350}
+        width={350}
+        alt="mahadevan"
+        />
+    )
+}
