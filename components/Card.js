43c0862070e0a5864ea60afba07fc41fb8ae23fb
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-04-13 20:48:53 +0530

    Add files via upload

--- components/Card.js
+++ components/Card.js
@@ -1,31 +1,37 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Skeleton from 'react-loading-skeleton'
+// import Skeleton from 'react-loading-skeleton'
+import Link from 'next/link'
 import React from 'react'
-export default function Card({item,iurl}){
+export default function Card({item,iurl,detail}){
     const url = `https://mahadevan.herokuapp.com/assets/${iurl}/${item.image}.svg`
+    // const url = `http://localhost:5000/assets/${iurl}/${item.image}.svg`
     console.log(url,'url')
+    const link = `/${detail}/${item.id}`
     return(
             <div className={styles.card}>
                 <div className={styles.cimage}>
-                    {<img src={url}/>||<Skeleton width={300} height={300}/>}
+                    {/* {<img src={url}/>||<Skeleton width={300} height={300}/>} */}
+                    <ImageComp isrc={url} />
                 </div>
                 <div className={styles.ccont}>
-                    <h2>{item.content || <Skeleton/>}</h2>
+                    <Link href={link}>
+                        <a><h2>{item.content}</h2></a>
+                    </Link>
                 </div>
             </div>      
     )
 }
 
 
-// const ImageComp= (props)=>{
-//     const url = `http://localhost:5000/assets/pimage/${props.isrc}.svg`
-//     return(
-//         <Image 
-//         src={url}
-//         height={350}
-//         width={350}
-//         alt="mahadevan"
-//         />
-//     )
-// }
+const ImageComp= (props)=>{
+    const url = props.isrc;
+    return(
+        <Image 
+        src={url}
+        height={350}
+        width={350}
+        alt="mahadevan"
+        />
+    )
+}
