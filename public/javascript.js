10af4b1894577ee1d813fc5c14d65c4b8e822ba3
MAHADEVAN-A <68148704+MAHADEVAN-A@users.noreply.github.com>
2021-04-14 18:28:48 +0530

    Add files via upload

--- public/javascript.js
+++ public/javascript.js
@@ -1,6 +1,6 @@
-const links = document.querySelectorAll('nav ul li')
-const ultag = document.querySelector('nav ul')
-const check = document.getElementById('check')
+var links = document.querySelectorAll('nav ul li')
+var ultag = document.querySelector('nav ul')
+var check = document.getElementById('check')
 links.forEach((item)=>{
     item.addEventListener('click',()=>{
         ultag.style.left='-100%';
@@ -8,14 +8,20 @@ links.forEach((item)=>{
     })
 })
 
-function handleChange(e){
-    if(ultag.style.left=='-100%'){
-        ultag.style.left='0';
-    }
-    else{
-        ultag.style.left='-100%';
-    }
-}
+// check.addEventListener('change',()=>{
+//     console.log('mahadevan');
+//     // if(ultag.style.left=='-100%'){
+//     //     ultag.style.left='0';
+//     // }
+//     // else{
+//     //     ultag.style.left='-100%';
+//     // }
+// })
+
+// function handleChange(e){
+//     console.log('mahadevan')
+// }
 console.log(links);
 console.log(check);
 console.log(ultag);
+
